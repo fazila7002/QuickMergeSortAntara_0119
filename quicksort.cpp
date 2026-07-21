9d78a0c2a4c5397b2b6320512b011ac83bf1acfc
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-21 15:14:38 +0700

    prosedur input

--- quicksort.cpp
+++ quicksort.cpp
@@ -6,3 +6,32 @@ int arr[20]; //inisialisasi variabel array untuk menampung nilai
 int cmp_count = 0; //inisialisasi variabel untuk jumlah perbandingan
 int mov_count = 0; //inisialisasi variabel untuk jumlah pergerakan data
 int n; //inisialisasi variabel n untuk panjang element array
+
+
+void input() //membuat prosedur input
+{
+    while (true) //looping untuk meminta input panjang element array
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20) //jika jumlah element kurang dari atau sama dengan 20 looping selesai
+        {
+            break;
+        }
+        else //jika jumlah element lebih dari 20 permintan input akan di ulang
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
+    cout << "\n--------------------------" << endl; //display untuk menampilkan permintaan mengisi masing masing index array
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n--------------------------" << endl;
+
+    for (int i = 0; i < n; i++) //looping untuk mengisi index array sampai index ke n-1
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
+}
