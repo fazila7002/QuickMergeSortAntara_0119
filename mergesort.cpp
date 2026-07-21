cd24836715e0f76fd55c4c9e7e4bb16a54af2b25
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-21 15:05:17 +0700

    proswdiur output dan main

--- mergesort.cpp
+++ mergesort.cpp
@@ -85,6 +85,23 @@ for (int x = low; x <= high; x++)
 {
     arr[x] = B[x];
 }
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    mergeSort(0, n - 1);
+    output();
+}
 }
 
 
