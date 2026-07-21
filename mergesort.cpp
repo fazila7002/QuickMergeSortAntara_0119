cb741a1422aa2a04e28b9b6138dfc9af5698e479
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-21 14:50:33 +0700

    memamnggil diri sendiri

--- mergesort.cpp
+++ mergesort.cpp
@@ -41,4 +41,9 @@ void mergeSort(int low, int high)
 
     int mid = (low + high) /2 ; //step2
 
+    //step3
+    //fungsi rekursi - memanggil diri sendiri
+    mergeSort(low, mid); //step 3.a
+    mergeSort(mid + 1,high); //step 3.b
+
 }
