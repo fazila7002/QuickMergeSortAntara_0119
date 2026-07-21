2541dbc7f8ecf19234818161cb0874b77f186d9e
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-21 14:43:25 +0700

    disini ada output input isi dari elementnya

--- mergesort.cpp
+++ mergesort.cpp
@@ -19,4 +19,10 @@ void input()
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+
+    cout << "\n--------------------------" << endl;
+    cout << "\nInputkan isi element array" << endl;
+    cout << "\n----------============----" << endl;
+
+    
 }
