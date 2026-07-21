5e816f79a8803d23720e5b27f69cacc7c7ca3607
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-21 14:41:14 +0700

    disini ada looping while jika benar n <=20 maka akan di lanjut

--- mergesort.cpp
+++ mergesort.cpp
@@ -7,5 +7,16 @@ int n; //disini n untuk array input size
 
 void input() 
 {
-    
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+        if (n <= 20)
+        {
+            break;
+        }
+        else{
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
 }
